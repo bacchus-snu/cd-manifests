af2e1216b72402d63ce4a0ccb10d721084a1a8c0
Yongun Seong <nevivurn@nevi.dev>
2024-03-11 06:34:38 +0900

    feat(sommelier): add ford

---
+++ talos/sommelier/patches/worker/generate-nvidia-installer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# ref: https://www.talos.dev/v1.6/talos-guides/install/boot-assets/
+
+img_id=$(curl -fsSL --data-binary @- https://factory.talos.dev/schematics <<EOF | jq -r .id
+customization:
+  systemExtensions:
+    officialExtensions:
+      - siderolabs/intel-ucode
+      - siderolabs/nvidia-container-toolkit
+      - siderolabs/nonfree-kmod-nvidia
+EOF
+)
+
+cat <<EOF > nvidia-installer.yaml
+machine:
+  install:
+    image: factory.talos.dev/installer/${img_id}:v1.6.5
+EOF
